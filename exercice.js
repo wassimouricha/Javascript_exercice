945682b342ac2d27371cf9bd11304866f71854d5
wassim <wbouricha5@gmail.com>
2021-12-07 16:46:18 +0100

    Exercice

---
+++ exercice.js
@@ -0,0 +1,65 @@
+
+//exercice 1
+var variableun = 1 ;
+var variabledeux = 2 ;
+var inversion ;
+
+inversion = variableun ;
+variableun =  variabledeux ;
+variabledeux = inversion;
+
+console.log( inversion);
+console.log( variableun);
+
+
+//exercice 2
+
+const nom = 'toto'
+const Age = '30'
+const homme = 'true'
+
+console.log(nom + ' à ' + Age + ' ans ' + ' et c est ' + homme);
+
+//exercice 3
+
+console.log(Math.pow(4, 2));
+
+//exercice 4
+
+function addition() {
+    console.log(4 + 2)
+};
+
+addition()
+
+//exercice 5
+
+
+
+function conversion() {
+    console.log(4 * 60),
+    console.log(3 * 60)
+};
+
+conversion();
+
+//exercice 6
+console.log('---')
+
+var x = 1
+    x++;
+ console.log(x);
+    x--;
+console.log(x);
+
+
+//exercice 7
+
+function triangle() {
+    console.log((8 * 2) / 2),
+    console.log((7 * 3) / 2)
+};
+
+triangle();
+
+//exercice 8
